47fe33f019c9bab4af21f822ba7fe40a81059679
camili costa <camili.costa07@gmail.com>
2023-10-27 20:34:59 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int numero;
+int numeroPar = 0;
+do
+{
+Console.WriteLine("Digite um numero inteiro ou positivo?");
+
+Int32.TryParse(Console.ReadLine(),out numero);
+}
+
+while (numero <= 0);
+{
+
+}
+Console.WriteLine("Essa é a sequencia de numeros pares até esse número:");
+while (numeroPar <= numero)
+{
+  
+    Console.Write ($" {numeroPar}");
+    numeroPar +=2;
+}
